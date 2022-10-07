787e185c7b5090958d1ed55d0bea29df4ce4e446
Kayla Justice <kaylarjustice@gmail.com>
2022-10-07 09:53:07 -0500

    local storage updates.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,3 @@
-/*  
-  OoO- 
-
-  1. User types in a city name
-    a. What if the city doesn't exist? (return or alert?)
-    b. What if there is a typo in the city name? (return or alert?)
-  
-  2. User clicks search button
-
-  3. Several actions are attached to the search button:
-    a. fetch request to the api to retrieve city-specific current and future forecasts
-    b. display of fetched data in current and future forecast <divs>
-    c. save searched city information to localStorage
-    d. create and append designated buttons to saved searches
-    e. user can access old searches with new buttons
-
-  4. Repeat
-*/
-
 // DOM selectors
 const apiKey = '64fff1a969c76e6a48c1adb0a5ffeb4e';
 const searchBtn = document.querySelector('#search-button');
@@ -26,18 +7,8 @@ const futureWeatherResults = document.getElementById('future-weather-content');
 
 let savedWeather = [];
 
-// const savedCity = 
-// const savedWeather =
-
-
-
-
-// Every time search, create button, set button's text to search, append button to div, eventListener that when clicked will 
-// when page loads, attempts to retrieve previous city searches, creates buttons
-
-
 // Retrieve current city weather data / fetch command
-function getCurrentWeather() {
+function getCurrentWeather(event) {
  
   let city = cityInput.value;
 
@@ -66,16 +37,17 @@ function getCurrentWeather() {
 
   // create buttons in here
   const button = document.createElement("button");
-  button.setAttribute("id", "city-button");
+  button.setAttribute("class", "city-button");
   button.innerHTML = city;
-  document.querySelector(".search-box").appendChild(button);  
+  document.querySelector(".search-box").appendChild(button);
 
-   
   button.addEventListener('click', function() {
-    // retrieve saved data
-    
+    event.target = button;
+  })
 }
 
+//event listener that sends city name to pre-existing function
+// if city-button is clicked, button's html as city, input field as city
 
 // Referenced mini-project-06 solution code
 // This function creates and displays the elements for the currentWeather section
@@ -88,7 +60,6 @@ function displayCurrentWeather(data) {
 
   // set up <div> to hold result content
   let resultCard = document.createElement('div');
-  // classList returns CSS names of an element; used alon it's read-only, but methods can be used
   resultCard.classList.add('card', 'bg-light', 'text-dark', 'mb-3', 'p-3');
 
   // create and append resultBody body <div> inside resultCard container <div>
@@ -156,6 +127,10 @@ function getFutureWeather() {
     .then(function (data) {
       console.log(data)
 
+      // This empties out results, allows for new data
+      // innerHTML removes any content inside of a targeted container
+      futureWeatherResults.innerHTML=""
+
       for (let i = 7; i < 40; i += 7) {
         console.log(data.list[i]);
 
@@ -170,11 +145,7 @@ function getFutureWeather() {
 function displayFutureWeather(data) {
   
   console.log(data); 
-
-  // This empties out results, allows for new data
-  // innerHTML removes any content inside of a targeted container
-  // futureWeatherResults.innerHTML=""
-     
+      
   // set up <div> to hold result content
   let resultCard = document.createElement('div');
   // classList returns CSS names of an element; used alon it's read-only, but methods can be used
@@ -225,100 +196,17 @@ function displayFutureWeather(data) {
   futureWeatherResults.append(resultCard);
 }
 
-
-
-
-
-
-
-// const saveWeather = {
-//   city: data.name,
-//   date: moment().format("MMM Do YYYY"),
-//   temperature: data.main.temp,
-//   humidity: data.main.humidity,
-//   wind: data.wind.speed,
-//   icon: data.weather[0].icon,
-//   description: data.weather[0].main
-// };
-
-// localStorage.setItem("saveWeather", JSON.stringify(saveWeather));
-
-// let city = localStorage.getItem("city");
-// console.log(city);
-
-// city.textContent = data.name
-
-// function storeWeather() {
-//   localStorage.setItem("saved-weather", JSON.stringify(savedWeather));
-// }
+savedWeather = JSON.parse(localStorage.getItem("saved-weather"));
 
 searchBtn.addEventListener('click', function() {
 
   // Click search button, results are saved to local storage
-
-  // If the search input was accepted,
-  if (cityInput === true) {
-
-    // Save contents of search to local storage
-    // Assign to city button
-
-  }
-  
-  storeWeather() {
-    savedWeather.push(cityInput + currentWeatherResults + futureWeatherResults);
+  // Save city to local storage
+  // Assign to city button
+   
+    savedWeather.push(cityInput.value);
     cityInput.value = "";
-    localStorage.setItem("saved-weather", JSON.stringify(savedWeather));
-  };
+    localStorage.setItem("saved-weather", JSON.stringify(savedWeather));  
   
   getCurrentWeather();
-  // storeWeather();
 });
-
-
-
-
-
-
-
-
-
-
-/* Code supplied by instructor
-const weatherDays = []  
-let currDay = null
-
-sampleData.list.forEach( function(timestampObj){
-
-  // Makes a moment date object for each record
-  const dateObj = moment.unix(timestampObj.dt)
-
-  // Generate the day # for the day in the date object
-  const dateNum = dateObj.format("DDD")
-
-  // If the current date in timestampObj hasn't had a record put into weatherDays, do that now 
-  // Then skip over all other records for this day
-  if( dateNum !== currDay && weatherDays.length < 5 ){
-    weatherDays.push( timestampObj )
-    currDay = dateNum
-  }
-
-})
-*/
-
-// when the seach button is clicked, it will look into this form field, call that api call and pass in city name
-// objects for each one of the items needed
-// parse json to get to it
-
-// each city is a button to retrieve data from local storage
-// currentweather can have city pass into it
-
-
-
-// localStorage for search history
-    // setItem, getItem
-    // tricky = types of data
-    // key = "string", value = stored as a string
-    // stringify/parse
-    // array
-
-// eventListener for search button, fetch request to grab api data and append things to screen (dynamically styled)
